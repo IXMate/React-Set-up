928ef08a2e36f630361135706774a5cc532c1121
Poramate <poramate.pro@gmail.com>
2025-03-20 15:23:52 +0700

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import AboutMe from "./components/AboutMe";
+
 export default function App() {
 
 
   return (
-    <>
-      <h1>Hi GenMateA</h1>
-    </>
+    <section className="bg-amber-50 ">
+    <div className="flex flex-col gap-5 justify-center items-center max-w-[1280px] ">
+    <h1 className="bg-amber-200 w-[250px] mx-auto"> React App Starter</h1>
+    <div>
+    <p className="text-xl bg-green-100 w-[250px] mx-auto">Tech Stack</p>
+    <ul className="bg-amber-200 w-[250px] mx-auto">
+        <li> vite</li>
+        <li> React</li>
+        <li> JavaScript</li>
+        <li> vite</li>
+      </ul>
+      <div className="bg-green-100 "> <AboutMe /> </div>
+    </div>
+    </div>
+    </section>
   )
 }
 
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function AboutMe() {
+  return (
+    <div>
+    <h1 className=' w-[120px] mx-auto'>Hi It's Mate</h1>
+    <img className='w-[120px] mx-auto'
+      src="https://uploads1.wikiart.org/images/isaac-levitan/portrait-of-nikolay-panafidin-1891.jpg!Large.jpg"
+      alt="Katherine Johnson"
+    />
+
+    </div>
+  )
+}
+
+export default AboutMe
+
+
+
+
